d6ddec8f58ae215768a6680fcf558c22e80a701b
Jeffrey Grimmie <Jeffreygrimmie@gmail.com>
2023-04-09 08:14:21 -0700

    Update dogebot.py
    Refactoring and updating code

--- dogebot.py
+++ dogebot.py
@@ -8,88 +8,94 @@ import yaml
 import tweepy
 
 
-def CurrentTime():
+def current_time():
     now = datetime.datetime.now()
     dt_string = now.strftime("%m/%d/%Y %H:%M:%S")
     return dt_string
+
+
 def tweet(status):
     auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
     auth.set_access_token(ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
     api = tweepy.API(auth)
     api.update_status(status)
-    
-def sendMail(mailSubject,mailBody):
+
+
+def send_mail(mail_subject, mail_body):
     with smtplib.SMTP("smtp.gmail.com", 587) as smtp:
-            smtp.ehlo()
-            smtp.starttls()
-            smtp.ehlo()
-            smtp.login(EMAIL_ADDRESS, EMAIL_PASSWORD)
-            subject = mailSubject
-            body = mailBody
-            msg = f"Subject: {subject}\n\n{body}"
-            smtp.sendmail(EMAIL_ADDRESS, EMAIL_ADDRESS, msg)
+        smtp.ehlo()
+        smtp.starttls()
+        smtp.ehlo()
+        smtp.login(EMAIL_ADDRESS, EMAIL_PASSWORD)
+        msg = f"Subject: {mail_subject}\n\n{mail_body}"
+        smtp.sendmail(EMAIL_ADDRESS, EMAIL_ADDRESS, msg)
+
+
 def delay():
     time.sleep(286)
-def getPrice():
-    url = 'https://pro-api.coinmarketcap.com/v1/cryptocurrency/quotes/latest' #coin market url
-    parameters = {'symbol': 'DOGE'}
-    headers = {'Accepts': 'application/json', 'X-CMC_PRO_API_KEY': API_KEY}
-    session = Session()
-    session.headers.update(headers)
-    response = session.get(url, params=parameters)
-    data = json.loads(response.text)
-    priceCurrent = (str(data['data']['DOGE']['quote']['USD']['price']))
-    #print ('24h Volume (USD):    ' + str(data['data']['DOGE']['quote']['USD']['volume_24h']))
-    #print ('Market Cap (USD):    ' + str(data['data']['DOGE']['quote']['USD']['market_cap']))
-    return priceCurrent
-def buy(amount): #number of doge coin's to buy
-    robin_stocks.orders.order_buy_crypto_by_quantity('DOGE', float(amount), timeInForce='gtc')
-def sell(amount):
-    robin_stocks.orders.order_sell_crypto_by_quantity('DOGE', float(amount), priceType='mark_price', timeInForce='gtc')
-def cancel(): #cancel all crypto orders
-    robin_stocks.orders.cancel_all_crypto_orders()
-def dollarToDoge(dollar, currentPrice):
-    conversion = doller / currentPrice
-    return conversion
-def dogeToDollar(doge, currentPrice):
-    conversion = doge * currentPrice
-    return conversion
 
-yaml.warnings({'YAMLLoadWarning': False})
-conf = yaml.safe_load(open('info.yml'))
-EMAIL_ADDRESS = conf['user']['email']
-EMAIL_PASSWORD = conf['user']['emailpassword']
-API_KEY = conf['user']['apikey']
-rhuser = conf['user']['rhusername']
-rhpass = conf['user']['rhpassword']
-CONSUMER_KEY = conf['user']['CONSUMER_KEY']
-CONSUMER_SECRET = conf['user']['CONSUMER_SECRET']
-ACCESS_TOKEN = conf['user']['ACCESS_TOKEN']
-ACCESS_TOKEN_SECRET = conf['user']['ACCESS_TOKEN_SECRET']
 
-login = robin_stocks.robinhood.login(rhuser, rhpass, expiresIn=86400, by_sms=True) #robinhood login
-currentBuyIn = 0.04 #price of doge on last pruchase
-currentHighSetPoint = 1.0
-
-while True:
+def get_price():
     try:
-        currentPrice = getPrice()
+        url = 'https://pro-api.coinmarketcap.com/v1/cryptocurrency/quotes/latest'
+        parameters = {'symbol': 'DOGE'}
+        headers = {'Accepts': 'application/json', 'X-CMC_PRO_API_KEY': API_KEY}
+        session = Session()
+        session.headers.update(headers)
+        response = session.get(url, params=parameters)
+        data = json.loads(response.text)
+        price_current = (str(data['data']['DOGE']['quote']['USD']['price']))
+        return price_current
     except Exception as e:
-        print(e)
-    if float(currentPrice) < currentBuyIn:
-        mailSubject = "Doge coin has dropped below current low threshold!"
-        mailBody = ('Current Price (USD): ' + str(currentPrice) + ' ' + str(CurrentTime()))
-        sendMail(mailSubject,mailBody)
-        tweet('Current Price (USD): ' + str(currentPrice) + ' ' + str(CurrentTime()))
-        print('Current Price (USD): ' + str(currentPrice) + ' ' + str(CurrentTime()))
-        delay()
-    elif float(currentPrice) > currentHighSetPoint:
-        mailSubject = "Doge coin has hit the current high set point check your app."
-        mailBody = ('Current Price (USD): ' + str(currentPrice) + ' ' + str(CurrentTime()))
-        sendMail(mailSubject,mailBody)
-        tweet('Current Price (USD): ' + str(currentPrice) + ' ' + str(CurrentTime()))
-        print('Current Price (USD): ' + str(currentPrice) + ' ' + str(CurrentTime()))
-        delay()
-    else:
-        print('Current Price (USD): ' + str(currentPrice) + ' ' + str(CurrentTime()))
-        delay()
+        print(f"Error fetching price: {e}")
+        return None
+
+
+def main():
+    yaml.warnings({'YAMLLoadWarning': False})
+    conf = yaml.safe_load(open('info.yml'))
+    global EMAIL_ADDRESS, EMAIL_PASSWORD, API_KEY, rhuser, rhpass
+    global CONSUMER_KEY, CONSUMER_SECRET, ACCESS_TOKEN, ACCESS_TOKEN_SECRET
+    EMAIL_ADDRESS = conf['user']['email']
+    EMAIL_PASSWORD = conf['user']['emailpassword']
+    API_KEY = conf['user']['apikey']
+    rhuser = conf['user']['rhusername']
+    rhpass = conf['user']['rhpassword']
+    CONSUMER_KEY = conf['user']['CONSUMER_KEY']
+    CONSUMER_SECRET = conf['user']['CONSUMER_SECRET']
+    ACCESS_TOKEN = conf['user']['ACCESS_TOKEN']
+    ACCESS_TOKEN_SECRET = conf['user']['ACCESS_TOKEN_SECRET']
+
+    login = robin_stocks.robinhood.login(rhuser, rhpass, expiresIn=86400, by_sms=True)
+    current_buy_in = 0.04
+    current_high_set_point = 1.0
+
+    while True:
+        current_price = get_price()
+        if current_price is None:
+            print("Failed to fetch price. Retrying in 5 minutes.")
+            time.sleep(300)
+            continue
+
+        if float(current_price) < current_buy_in:
+            mail_subject = "Doge coin has dropped below current low threshold!"
+            mail_body = f"Current Price (USD): {current_price} {current_time()}"
+            send_mail(mail_subject, mail_body)
+            tweet(f"Current Price (USD): {current_price} {current_time()}")
+            print(f"Current Price (USD): {current_price} {current_time()}")
+            delay()
+
+        elif float(current_price) > current_high_set_point:
+            mail_subject = "Doge coin has hit the current high set point check your app."
+            mail_body = f"Current Price (USD): {current_price} {current_time()}"
+            send_mail(mail_subject, mail_body)
+            tweet(f"Current Price (USD): {current_price} {current_time()}")
+            print(f"Current Price (USD): {current_price} {current_time()}")
+            delay()
+
+        else:
+            print(f"Current Price (USD): {current_price} {current_time()}")
+            delay()
+
+if __name__ == "__main__":
+    main()
